f14e1f9662455ddc8bfe08ca217d686283a8ba0f
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-09-25 19:44:33 +0100

    comments and likes

--- backend/01blog/src/main/java/com/example/demo/models/Like.java
+++ backend/01blog/src/main/java/com/example/demo/models/Like.java
@@ -12,19 +12,21 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "likes", uniqueConstraints = @UniqueConstraint(name = "unique_like", columnNames = {"creator_id", "post_id"}))
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Like {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "creator_id", nullable = false)
 	private User creator;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "post_id", nullable = false)
 	private Post post;
 
