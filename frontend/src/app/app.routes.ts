0dd750f4ee46f2a7642cdfe54b3f168eb02c8f26
youssefhajjaoui <youssefhajjaoui67@gmail.com>
2025-10-11 17:52:53 +0100

    add posts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { Auth } from './auth/auth/auth.component';
+import { Post } from './home/posts/Post/post.component';
+import { AuthGuard } from './services/auth.guard';
 
 export const routes: Routes = [
   { path: 'auth', component: Auth },
-  { path: '', redirectTo: '/auth', pathMatch: 'full' }
+  { path: '' , component: Post , canActivate: [AuthGuard] },
+  // { path: '', redirectTo: '/auth', pathMatch: 'full' }
 ];
